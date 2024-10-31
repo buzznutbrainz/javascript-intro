c0f0d4de2bb1117be459769e76423ae6033a734f
buzznutbrainz <ava.ehtesham-heravi@edu.sait.ca>
2024-10-31 16:37:34 -0600

    everything

--- intro.js
+++ intro.js
@@ -3,3 +3,16 @@ secret.addEventListener('click',function(){
 
 
 })
+light_on.addEventListener('click',function(){
+    console.log("let there be light");
+    document.getElementById("lightbulb").src="lightbulb_on.webp";
+
+
+})
+
+light_off.addEventListener("click",function(){
+    document.getElementById("lightbulb").src="lightbulb_off.webp";
+
+})
+
+//When the user clicks the 'light_on' button, change the 'lightbulb' image to show the lightbulb on 
